45a9034bcec4807f3ab0f2fcaa1734a3231f0193
Otto Vogrincic <ovogrin@gmail.com>
2017-10-26 23:29:38 -0400

    updated fitness tracking site for midterm

---
+++ server/appObject.js
@@ -0,0 +1,27 @@
+module.exports = {
+    pictures: [
+        "bicycle.jpg",
+        "burpees.jpg",
+        "crunches.jpg",
+        "lunges.jpg",
+        "plank.jpg",
+        "pushups.jpg",
+        "running.jpeg",
+        "squats.jpg",
+        "swimming.jpg",
+        "walking.jpeg"
+    ],
+    quotes: [
+        { text: "Bicycle" },
+        { text: "Burpees" },
+        { text: "Crunches" },
+        { text: "Lunges" },
+        { text: "Plank" },
+        { text: "Pushups" },
+        { text: "Running" },
+        { text: "Squats" },
+        { text: "Swimming" },
+        { text: "Walking" },
+    ]
+    
+}
